cedc635b6911c71ea6eab2d6cb9228cd9f3ae593
robertolatini31 <robertolatini31@gmail.com>
2022-05-03 14:54:10 +0200

    added content section

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
    <SiteHeader />
+   <SiteMain />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import SiteHeader from './components/SiteHeader.vue'
-
+import SiteMain from './components/SiteMain.vue'
 export default {
   name: 'App',
   components: {
     // HelloWorld
     SiteHeader,
-    
+    SiteMain,
   }
 }
 </script>
---
+++ src/components/SiteMain.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <div class="container">
+        <h3> --> Content goes here</h3>
+    </div>
+    <!-- /.container -->
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'SiteMain'
+}
+</script>
+
+<style lang="scss" scoped>
+h3 {
+    padding: 3rem 0;
+}
+main {
+    background-color: black;
+    color: white;
+    padding: 1rem;
+}
+ 
+</style>
